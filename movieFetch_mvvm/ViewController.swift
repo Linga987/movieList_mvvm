87183aeb4829adcd8ca72b25fc9b785339149d1d
shanmugakrazybee <shanmuga.srinivas@krazybee.com>
2024-05-14 21:54:25 +0530

    Initial Commit

---
+++ movieFetch_mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieFetch_mvvm
+//
+//  Created by shanmuga srinivas on 14/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
